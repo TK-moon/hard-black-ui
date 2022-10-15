800482cc3ac534f644f41f43e8ecb1ecc00e92e9
TK-moon <tkmoon.dev@gmail.com>
2022-10-15 14:03:49 +0900

    :wrench: ESLint 설정 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,48 @@
 module.exports = {
-  extends: ['plugin:react-hooks/recommended', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
+    'plugin:storybook/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    sourceType: 'module',
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        aspects: ['invalidHref', 'preferButton'],
-      },
-    ],
     'no-redeclare': 'off',
     '@typescript-eslint/no-redeclare': ['error'],
     '@typescript-eslint/no-unnecessary-condition': 'warn',
     '@typescript-eslint/no-unused-vars': 'error',
   },
   settings: {
+    'react': {
+      version: 'detect',
+    },
     'import/resolver': {
       typescript: {
         project: __dirname,
       },
     },
   },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+  ignorePatterns: ['.eslintrc.js'],
 };
