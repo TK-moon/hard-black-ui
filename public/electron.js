9e06f1cdd83e599e841aa36ab64e8a445ae864d3
TK-moon <tkmoon.dev@gmail.com>
2022-10-15 13:09:12 +0900

    :hammer: MAC에서 일렉트론 빌드

--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,6 @@
 const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+
 const path = require('path');
 function createWindow() {
   const win = new BrowserWindow({
@@ -9,11 +11,22 @@ function createWindow() {
       contextIsolation: false,
     },
   });
-  win.loadURL('http://localhost:6006');
+
+  const start_url = isDev
+    ? 'http://localhost:6006'
+    : `file://${path.join(__dirname, '../storybook-static/index.html')}`;
+
+  if (isDev) {
+    win.loadURL('http://localhost:6006');
+  } else {
+    win.loadURL(`file://${path.join(__dirname, '../storybook-static/index.html')}`);
+  }
 }
+
 app.whenReady().then(() => {
   createWindow();
 });
+
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit();
 });
